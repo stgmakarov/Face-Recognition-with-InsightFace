6498988010ee7a2b0b7355dedb17275430efd2a8
Stanislav Makarov <stasgmakarov@gmail.com>
2022-11-27 20:44:07 +0300

    fix:исправления по новым версиям компонет + русские шрифты

--- src/recognizer_stream.py
+++ src/recognizer_stream.py
@@ -1,4 +1,5 @@
 import sys
+
 sys.path.append('../insightface/deploy')
 sys.path.append('../insightface/src/common')
 
@@ -14,19 +15,19 @@ import time
 import dlib
 import cv2
 import os
+
 os.environ['CUDA_VISIBLE_DEVICES'] = "0"
 
 ap = argparse.ArgumentParser()
 
 ap.add_argument("--mymodel", default="outputs/my_model.h5",
-    help="Path to recognizer model")
+                help="Path to recognizer model")
 ap.add_argument("--le", default="outputs/le.pickle",
-    help="Path to label encoder")
+                help="Path to label encoder")
 ap.add_argument("--embeddings", default="outputs/embeddings.pickle",
-    help='Path to embeddings')
+                help='Path to embeddings')
 ap.add_argument("--video-out", default="../datasets/videos_output/stream_test.mp4",
-    help='Path to output video')
-
+                help='Path to output video')
 
 ap.add_argument('--image-size', default='112,112', help='')
 ap.add_argument('--model', default='../insightface/models/model-y1-test2/model,0', help='path to load model.')
@@ -49,10 +50,11 @@ labels = le.fit_transform(data['names'])
 detector = MTCNN()
 
 # Initialize faces embedding model
-embedding_model =face_model.FaceModel(args)
+embedding_model = face_model.FaceModel(args)
 
 # Load the classifier model
 model = load_model(args.mymodel)
+
 
 # Define distance function
 def findCosineDistance(vector1, vector2):
@@ -65,7 +67,8 @@ def findCosineDistance(vector1, vector2):
     a = np.dot(vec1.T, vec2)
     b = np.dot(vec1.T, vec1)
     c = np.dot(vec2.T, vec2)
-    return 1 - (a/(np.sqrt(b)*np.sqrt(c)))
+    return 1 - (a / (np.sqrt(b) * np.sqrt(c)))
+
 
 def CosineSimilarity(test_vec, source_vecs):
     """
@@ -74,7 +77,8 @@ def CosineSimilarity(test_vec, source_vecs):
     cos_dist = 0
     for source_vec in source_vecs:
         cos_dist += findCosineDistance(test_vec, source_vec)
-    return cos_dist/len(source_vecs)
+    return cos_dist / len(source_vecs)
+
 
 # Initialize some useful arguments
 cosine_threshold = 0.8
@@ -89,8 +93,8 @@ cap = cv2.VideoCapture(0)
 frame_width = int(cap.get(3))
 frame_height = int(cap.get(4))
 save_width = 600
-save_height = int(600/frame_width*frame_height)
-video_out = cv2.VideoWriter(args.video_out, cv2.VideoWriter_fourcc('M','J','P','G'), 30, (save_width,save_height))
+save_height = int(600 / frame_width * frame_height)
+video_out = cv2.VideoWriter(args.video_out, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 30, (save_width, save_height))
 
 while True:
     ret, frame = cap.read()
@@ -98,7 +102,7 @@ while True:
     rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     frame = cv2.resize(frame, (save_width, save_height))
 
-    if frames%3 == 0:
+    if frames % 3 == 0:
         trackers = []
         texts = []
 
@@ -110,15 +114,17 @@ while True:
             reco_tick = time.time()
             for bboxe in bboxes:
                 bbox = bboxe['box']
-                bbox = np.array([bbox[0], bbox[1], bbox[0]+bbox[2], bbox[1]+bbox[3]])
+                bbox = np.array([bbox[0], bbox[1], bbox[0] + bbox[2], bbox[1] + bbox[3]])
                 landmarks = bboxe['keypoints']
-                landmarks = np.array([landmarks["left_eye"][0], landmarks["right_eye"][0], landmarks["nose"][0], landmarks["mouth_left"][0], landmarks["mouth_right"][0],
-                                     landmarks["left_eye"][1], landmarks["right_eye"][1], landmarks["nose"][1], landmarks["mouth_left"][1], landmarks["mouth_right"][1]])
-                landmarks = landmarks.reshape((2,5)).T
+                landmarks = np.array([landmarks["left_eye"][0], landmarks["right_eye"][0], landmarks["nose"][0],
+                                      landmarks["mouth_left"][0], landmarks["mouth_right"][0],
+                                      landmarks["left_eye"][1], landmarks["right_eye"][1], landmarks["nose"][1],
+                                      landmarks["mouth_left"][1], landmarks["mouth_right"][1]])
+                landmarks = landmarks.reshape((2, 5)).T
                 nimg = face_preprocess.preprocess(frame, bbox, landmarks, image_size='112,112')
                 nimg = cv2.cvtColor(nimg, cv2.COLOR_BGR2RGB)
-                nimg = np.transpose(nimg, (2,0,1))
-                embedding = embedding_model.get_feature(nimg).reshape(1,-1)
+                nimg = np.transpose(nimg, (2, 0, 1))
+                embedding = embedding_model.get_feature(nimg).reshape(1, -1)
 
                 text = "Unknown"
 
@@ -138,7 +144,7 @@ while True:
                 if cos_similarity < cosine_threshold and proba > proba_threshold:
                     name = le.classes_[j]
                     text = "{}".format(name)
-                    print("Recognized: {} <{:.2f}>".format(name, proba*100))
+                    print("Recognized: {} <{:.2f}>".format(name, proba * 100))
                 # Start tracking
                 tracker = dlib.correlation_tracker()
                 rect = dlib.rectangle(bbox[0], bbox[1], bbox[2], bbox[3])
@@ -148,9 +154,9 @@ while True:
 
                 y = bbox[1] - 10 if bbox[1] - 10 > 10 else bbox[1] + 10
                 cv2.putText(frame, text, (bbox[0], y), cv2.FONT_HERSHEY_COMPLEX, 0.45, (0, 0, 255), 2)
-                cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (255,0,0), 2)
+                cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (255, 0, 0), 2)
     else:
-        for tracker, text in zip(trackers,texts):
+        for tracker, text in zip(trackers, texts):
             pos = tracker.get_position()
 
             # unpack the position object
@@ -159,8 +165,8 @@ while True:
             endX = int(pos.right())
             endY = int(pos.bottom())
 
-            cv2.rectangle(frame, (startX, startY), (endX, endY), (255,0,0), 2)
-            #cv2.putText(frame, text, (startX, startY - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0,0,255),2)
+            cv2.rectangle(frame, (startX, startY), (endX, endY), (255, 0, 0), 2)
+            # cv2.putText(frame, text, (startX, startY - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0,0,255),2)
             cv2.putText(frame, text, (startX, startY - 15), cv2.FONT_HERSHEY_COMPLEX, 0.45, (0, 0, 255), 2)
 
     cv2.imshow("Frame", frame)
